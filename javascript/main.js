66406df1d792860138dbb5ab6e5576caee221fc2
swardinfo <sward1000@gmail.com>
2022-12-22 10:49:49 +1000

    Import header and footer into index

---
+++ javascript/main.js
@@ -0,0 +1,10 @@
+
+
+
+/**
+ * Imports external menu and footer HTML into a page
+ */
+$(document).ready(function(){
+    $( "nav" ).load( "/html/menu.html" );
+    $( "footer" ).load( "/html/footer.html" );
+ });
