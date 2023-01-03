43193339cd877b7dd7ad6e05edc9cd6154ca935d
swardinfo <sward1000@gmail.com>
2023-01-03 14:17:45 +1100

    Content and styling updated

--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,13 @@
 const mediaQuery = window.matchMedia("(min-width: 1000px)");
 const menuButton = document.querySelector("#menu-button");
 const menu = document.querySelector("#menu");
+const openHelpVideoLink = document.querySelector("#open-help-video-link");
+const closeHelpVideoButton = document.querySelector("#close-help-video-button");
+const helpVideoContainer = document.querySelector("#help-video-container");
+const videoOverlay = document.querySelector("#video-overlay");
+const contactMeCheckbox = document.querySelector("#contact-me");
+const emailContactRadioButton = document.querySelector("#email-contact");
+const phoneContactRadioButton = document.querySelector("#phone-contact");
 
 /**
  * Imports external menu and footer HTML into a page
@@ -40,11 +47,40 @@ document.addEventListener("click", function(e)  {
  * Handles the menuButton click event.
  */
 menuButton.addEventListener("click", () => {
-    if (menu.classList.contains("menu-open")) {
+    if (menu.classList.contains("show")) {
         closeMenu();
     } else {
         openMenu();
     }
+});
+
+/**
+ * Handles the openHelpVideoLink click event.
+ */
+openHelpVideoLink.addEventListener("click", function(e) {
+    showVideoContainer(helpVideoContainer);
+});
+
+/**
+ * Handles the openHelpVideoLink mouseover event.
+ */
+openHelpVideoLink.addEventListener("mouseover", function(e) {
+    showVideoContainer(helpVideoContainer);
+});
+
+/**
+ * Handles the closeHelpVideoButton click event.
+ */
+closeHelpVideoButton.addEventListener("click", function(e) {
+    closeVideoContainer(helpVideoContainer);
+});
+
+/**
+ * Handles the click event for the contactMeCheckbox
+ */
+contactMeCheckbox.addEventListener("click", function(e) {
+    emailContactRadioButton.disabled = !contactMeCheckbox.checked;
+    phoneContactRadioButton.disabled = !contactMeCheckbox.checked;
 });
 
 /**
@@ -61,18 +97,35 @@ function handleScreenSizeChange(e) {
 }
 
 /**
- * Opens the navigation menu.
+ * Adds a class to a navigation menu container indicating it should be opened.
  */
 function openMenu() {
     menuButton.setAttribute("aria-expanded", true);
-    menu.classList.add("menu-open");
+    menu.classList.add("show");
 }
 
 /**
- * Closes the navigation menu.
+ * Removes a class from a navigation menu container indicating it should be closed.
  */
 function closeMenu() {
     menuButton.setAttribute("aria-expanded", false);
-    menu.classList.remove("menu-open");
+    menu.classList.remove("show");
 }
 
+/**
+ * Adds a class to a video container indicating it should be opened.
+ * @param {string} videoContainer - The video container to open.
+ */
+function showVideoContainer(videoContainer) {
+    videoContainer.classList.add("show");
+    videoOverlay.classList.add("show");
+}
+
+/**
+ * Removes a class from a video container indicating it should be closed.
+ * @param {string} videoContainer - The video container to close.
+ */
+function closeVideoContainer(videoContainer) {
+    videoContainer.classList.remove("show");
+    videoOverlay.classList.remove("show");
+}
